26ee2fae082f55a7a8f42e83d5e00f399059dbd2
Thomas.Finnern <GitProjects@insidethemachine.de>
2015-05-31 21:55:20 +0200

    Improved edit image xml fields

--- admin/views/Image/tmpl/edit.php
+++ admin/views/Image/tmpl/edit.php
@@ -11,23 +11,26 @@
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 
-JHtml::_('behavior.tooltip');
-JHtml::_('behavior.formvalidation');
-JHtml::_('behavior.keepalive');
+//JHtml::_('behavior.tooltip');
+//JHtml::_('behavior.formvalidation');
+//JHtml::_('behavior.keepalive');
 
+JHtml::_('behavior.formvalidator');
+
+/**
 $script = "
 	Joomla.submitbutton = function(task) {
 		if (document.formvalidator.isValid(document.id('adminForm'))) {
 			// Basic validation ok (input name required):
 			// Validate gallery_id
 			if (document.adminForm.gallery_id.value <= 0){ //'document'+form name+input name+'value'
-				alert('<?php echo JText::_('COM_RSGALLERY2_YOU_MUST_SELECT_A_GALLERY');?>');
+				alert('<?php echo JText::_('COM_RSG2_YOU_MUST_SELECT_A_GALLERY');?>');
 				return;
 			}
 			Joomla.submitform(task, document.getElementById('adminForm'));
 		} else {
 			if (document.adminForm.title.value == ''){
-				alert('<?php echo JText::_('COM_RSGALLERY2_PLEASE_PROVIDE_A_VALID_IMAGE_TITLE');?>');
+				alert('<?php echo JText::_('COM_RSG2_PLEASE_PROVIDE_A_VALID_IMAGE_TITLE');?>');
 				return;
 			}
 		}
@@ -35,53 +38,78 @@ $script = "
 ";
 $doc = JFactory::getDocument();
 $doc->addScriptDeclaration($script);
-
+/**/
 ?>
 
-<form action="<?php echo JRoute::_('index.php?option=com_rsg2&view=image&layout=edit&id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm"  class="form-validate">
+<form action="<?php echo JRoute::_('index.php?option=com_rsg2&view=image&layout=edit&id='.(int) $this->item->id); ?>"
+      method="post" name="adminForm" id="adminForm"  class="form-validate">
 
-	<div class="width-60 fltlft">
-		<fieldset class="adminform">
-		
-			<!-- legend>
-				<?php echo JText::_( 'COM_RSG2_IMAGE' ); ?>
-				<?php echo $this->item->id ? JText::_('COM_RSG2_EDIT') : JText::_('COM_RSG2_NEW');?>
-			</legend -->
-					
-			<!-- tr>
-				<td align="right">
-				<?php echo JText::_('COM_RSGALLERY2_PARENT_ITEM');?>
-				</td>
-				<td>
-				<?php echo $lists['parent']; ?>
-				</td>
-			</tr -->
+    <div class="form-horizontal">
+        <?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'edit')); ?>
 
-			<!--
-			<ul class="adminformlist">
-				<?php foreach($this->form->getFieldset() as $field): ?>
-					<li><?php echo $field->label; echo $field->input;?></li>
-				<?php endforeach; ?>
-			</ul>
-			-->
-			<div class="adminformlist">
-				<?php foreach($this->form->getFieldset() as $field): ?>
-					<div class="control-group">
-						<div class="control-label">
-							<?php echo $field->label; ?>
-						</div>
-						<div class="controls">
-							<?php echo $field->input; ?>
-						</div>
-					</div>
-				<?php endforeach; ?>
-			</div>
-			
-		</fieldset>
-	</div>
-	<div>
-		<input type="hidden" name="task" value="image.edit" />
-		<?php echo JHtml::_('form.token'); ?>
-	</div>
+        <?php echo JHtml::_('bootstrap.addTab', 'myTab', 'edit',
+            empty($this->item->id) ? JText::_('COM_RSG2_NEW', true) : JText::_('COM_RSG2_EDIT', true)); ?>
+        <div class="row-fluid">
+            <div class="span6">
+                <div class="row-fluid form-horizontal-desktop">
+                    <div class="Xadminformlist">
+                        <?php foreach($this->form->getFieldset() as $field):
+                            // echo $field->name.': "' . json_encode($field) . '""';
+                            switch ($field->name)
+                            {
+                                case 'jform[published]':
+                                    echo 'field: ' . json_encode($field);
+                                    echo 'input: ' . html_entity_decode(json_encode($field->input));
+                                    break;
+
+                                // Standard field output
+                                default:
+                                    ?>
+                                    <div class="control-group">
+                                            <div class="control-label">
+                                                <?php echo $field->label; ?>
+                                        </div>
+                                        <div class="controls">
+                                            <?php echo $field->input; ?>
+                                        </div>
+                                    </div>
+                                    <?php
+                                break;
+
+                            }
+                            ?>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="span3">
+                <h4><?php echo JText::_('COM_RSG2_ITEM_PREVIEW')?></h4>
+                        ToDo: Item preview<br>
+
+                <h4><?php echo JText::_('COM_RSG2_PARAMETERS')?></h4>
+                        ToDo: paramter not known<br>
+
+                <h4><?php echo JText::_('COM_RSG2_LINKS_TO_IMAGE')?></h4>
+                        ToDo: Links
+            </div>
+        </div>
+        <?php echo JHtml::_('bootstrap.endTab'); ?>
+
+        <?php echo JHtml::_('bootstrap.addTab', 'myTab', 'misc', JText::_('ACL', true)); ?>
+        <div class="row-fluid form-horizontal-desktop">
+            <div class="form-vertical">
+                <?php echo $this->form->renderField('misc'); ?>
+                Test 4
+            </div>
+        </div>
+        <?php echo JHtml::_('bootstrap.endTab'); ?>
+
+
+        <?php echo JHtml::_('bootstrap.endTabSet'); ?>
+    </div>
+
+
+    <input type="hidden" name="task" value="" />
+    <?php echo JHtml::_('form.token'); ?>
 </form>
 
