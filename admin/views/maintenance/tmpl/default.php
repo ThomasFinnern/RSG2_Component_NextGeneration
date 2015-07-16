968745fb903fd1016876f0d59af4a323a3a7c615
ThomasFinnern <noreplay@tomfinnern.de>
2015-07-16 13:16:48 +0200

    Mainten. next step

--- admin/views/maintenance/tmpl/default.php
+++ admin/views/maintenance/tmpl/default.php
@@ -20,23 +20,28 @@ $doc->addStyleSheet (JURI::root(true)."/administrator/components/com_rsg2/css/Ma
 function quickiconBar( $link, $image, $title, $text = "" ) {
     ?>
     <div style="float:left;">
-    <div class="icon-bar">
-        <a href="<?php echo $link; ?>">
-            <div class="iconimage">
-                <div class="rsg2-icon"><img src="<?php echo JURI_SITE;?>/administrator/components/com_rsgallery2/images/<?php echo $image;?>" alt="alternate text" /></div>
-                <div class="rsg2-text">
-                    <span class="maint-title"><?php echo $title;?></span>
-                    <span class="maint-text"><?php echo $text;?></span></div>
-            </div>
-        </a>
-    </div>
+		<div class="icon-bar">
+			<a href="<?php echo $link; ?>">
+				<div class="iconimage">
+					<div class="rsg2-icon">
+						<!--img src="<?php echo JURI_SITE;?>/administrator/components/COM_RSG2/images/<?php echo $image;?>" alt="alternate text" /-->
+						<?php echo JHtml::image('administrator/components/COM_RSG2/images/'.$image, $text); ?>						
+					</div>
+					<div class="rsg2-text">
+						<span class="maint-title"><?php echo $title;?></span>
+						<span class="maint-text"><?php echo $text;?></span>
+					</div>
+				</div>
+			</a>
+		</div>
     </div>
     <div class='rsg2-clr'>&nbsp;</div>
+<?php
 }
-
 ?>
 
-<form action="<?php echo JRoute::_('index.php?option=com_rsgallery2&view=maintenance'); ?>" method="post" name="adminForm" id="adminForm">
+<form action="<?php echo JRoute::_('index.php?option=COM_RSG2&view=maintenance'); ?>"
+      method="post" name="adminForm" id="adminForm">
 
 <?php if (!empty( $this->sidebar)) : ?>
 	<div id="j-sidebar-container" class="span2">
@@ -48,77 +53,46 @@ function quickiconBar( $link, $image, $title, $text = "" ) {
 <?php endif;?>
 
 		<div id="rsg2-thisform">
-		<div id='cpanel'>
-			<p>
-				<?php
-				$link = 'index.php?option=com_rsgallery2&amp;rsgOption=maintenance&amp;task=consolidateDB';
-				quickiconBar( $link, 'blockdevice.png',
-					JText::_('COM_RSGALLERY2_MAINT_CONSOLDB'), JText::_('COM_RSGALLERY2_MAINT_CONSOLDB_TXT') );
-				?>
-				<br>
-				<br>
-				<br>
-				<br>
-			</p>
-			<p>
-				<?php
-				$link = 'index.php?option=com_rsgallery2&amp;rsgOption=maintenance&amp;task=regenerateThumbs';
-				quickiconBar( $link, 'menu.png',
-					JText::_('COM_RSGALLERY2_MAINT_REGEN'), JText::_('COM_RSGALLERY2_MAINT_REGEN_TXT') );
-				?>
-				<br>
-				<br>
-				<br>
-				<br>
-			</p>
-			<p>
-				<?php
-				$link = 'index.php?option=com_rsgallery2&amp;rsgOption=maintenance&amp;task=optimizeDB';
-				quickiconBar( $link, 'db_optimize.png', JText::_('COM_RSGALLERY2_MAINT_OPTDB'),
-					JText::_('COM_RSGALLERY2_MAINT_OPTDB_TXT') );
-				?>
-				<br>
-				<br>
-				<br>
-				<br>
-			</p>
-		</div>
-		<div class='rsg2-clr'>&nbsp;</div>
-		</div>
+            <div id='cpanel'>
+                <p>
+                    <?php
+                    $link = 'index.php?option=COM_RSG2&amp;rsgOption=maintenance&amp;task=consolidateDB';
+                    quickiconBar( $link, 'blockdevice.png',
+                        JText::_('COM_RSG2_MAINT_CONSOLDB'), JText::_('COM_RSG2_MAINT_CONSOLDB_TXT') );
+                    ?>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                </p>
+                <p>
+                    <?php
+                    $link = 'index.php?option=COM_RSG2&amp;rsgOption=maintenance&amp;task=regenerateThumbs';
+                    quickiconBar( $link, 'menu.png',
+                        JText::_('COM_RSG2_MAINT_REGEN'), JText::_('COM_RSG2_MAINT_REGEN_TXT') );
+                    ?>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                </p>
+                <p>
+                    <?php
+                    $link = 'index.php?option=COM_RSG2&amp;rsgOption=maintenance&amp;task=optimizeDB';
+                    quickiconBar( $link, 'db_optimize.png', JText::_('COM_RSG2_MAINT_OPTDB'),
+                        JText::_('COM_RSG2_MAINT_OPTDB_TXT') );
+                    ?>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                </p>
+            </div>
+            <div class='rsg2-clr'>&nbsp;</div>
 
 
 
-
-
-		<table width="500">
-		<tr>
-			<td>
-			<table class="adminform">
-				<tr>
-					<td valign="top" width="300"><p><?php echo JText::_('COM_RSGALLERY2_SELECT_GALLERIES_TO_REGENERATE_THUMBNAILS_FROM')?></p></td>
-					<td valign="top">
-						<fieldset>
-						<legend><?php echo JText::_('COM_RSGALLERY2_SELECT_GALLERY')?></legend>
-						<?php echo $lists['gallery_dropdown'];?>
-						</fieldset>
-						<p>
-							<?php echo JText::sprintf('COM_RSGALLERY2_NEW_WIDTH_DISPLAY', $rsgConfig->get('image_width'))?>
-							<br>
-							<?php echo JText::sprintf('COM_RSGALLERY2_NEW_WIDTH_THUMB', $rsgConfig->get('thumb_width'))?>
-						</p>
-					</td>
-					<td width="10">&nbsp;</td>
-				</tr>
-				<tr>
-					<td colspan="3"></td>
-				</tr>
-			</table>
-			</td>
-		</tr>
-		</table>
-
-
-
+        </div>
 
         <div>
 			<input type="hidden" name="option" value="com_rsg2" />
